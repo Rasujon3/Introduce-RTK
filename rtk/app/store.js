40c2e6da262dc8228b24d1f6431e5f927e63f4e8
Rasujon3 <rasujon3@gmail.com>
2022-08-28 01:50:09 +0600

    5.5 Add new feature - dynamic counter

--- rtk/app/store.js
+++ rtk/app/store.js
@@ -1,10 +1,12 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const counterReduceer = require("../features/counter/counterSlice");
+const dynamicCounterReducer = require("../features/dynamicCounter/dynamicCounterSlice");
 
 // configure store
 const store = configureStore({
   reducer: {
     counter: counterReduceer,
+    dynamicCounter: dynamicCounterReducer,
   },
 });
 
