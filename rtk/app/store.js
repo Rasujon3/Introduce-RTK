52d6a3b1ea9793b8ffb9be0862b6eb7d3a6b7e51
Rasujon3 <rasujon3@gmail.com>
2022-08-28 02:00:04 +0600

    5.6 Middeware

--- rtk/app/store.js
+++ rtk/app/store.js
@@ -1,13 +1,16 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
 const counterReduceer = require("../features/counter/counterSlice");
 const dynamicCounterReducer = require("../features/dynamicCounter/dynamicCounterSlice");
+const { createLogger } = require("redux-logger");
 
+const logger = createLogger();
 // configure store
 const store = configureStore({
   reducer: {
     counter: counterReduceer,
     dynamicCounter: dynamicCounterReducer,
   },
+  middleware: (getDefaultMiddlewares) => getDefaultMiddlewares().concat(logger),
 });
 
 module.exports = store;
